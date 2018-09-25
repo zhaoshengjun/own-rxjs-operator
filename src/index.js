54a64bc4da762f2bd30258b5b11adc4c2fcbadad
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-25 21:54:54 +1000

    extract to a subscriber

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { from } from "rxjs";
 
 const observable$ = from([1, 2, 3, 4, 5]);
-
-observable$.subscribe({
+const subscriber = {
   next: value => console.log(value),
   complete: _ => console.log("Done!"),
   error: err => console.trace(err)
-});
+};
+observable$.subscribe(subscriber);
