ae48e3225666f9ecdf19db8023535dd87359f23a
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-25 21:53:59 +1000

    pass object instead of function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { from } from "rxjs";
 
 const observable$ = from([1, 2, 3, 4, 5]);
 
-observable$.subscribe(value => {
-  console.log(value);
+observable$.subscribe({
+  next: value => console.log(value),
+  complete: _ => console.log("Done!"),
+  error: err => console.trace(err)
 });
