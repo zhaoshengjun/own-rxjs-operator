d214c8e9619d27fb755afbca1b9d3839f47c1b4a
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-26 23:01:00 +1000

    create own map

---
+++ src/map.js
@@ -0,0 +1,1 @@
+export const map = fn => source => fn(source);
--- src/multiply.js
+++ src/multiply.js
@@ -1,3 +1,3 @@
-import { map } from "rxjs/operators";
+import { map } from "./map";
 
 export const multiply = number => map(value => value * number);
