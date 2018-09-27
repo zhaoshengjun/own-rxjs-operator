603c1df33837d985d939088b06e138525fb5b7cf
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-27 22:13:17 +1000

    use mergeMap

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import { fromEvent, of, pipe } from "rxjs";
-import { scan, delay } from "rxjs/operators";
+import { scan, delay, mergeMap } from "rxjs/operators";
 
-const observable$ = fromEvent(document, "click").pipe(scan(i => i + 1, 0));
+const observable$ = fromEvent(document, "click").pipe(
+  scan(i => i + 1, 0),
+  mergeMap(value => of(value).pipe(delay(500)))
+);
 
 const subscriber = {
   next: value => {
     console.log(value);
-    of(value)
-      .pipe(delay(500))
-      .subscribe({
-        next: value => console.log(`inner ${value}`)
-      });
   },
   complete: () => console.log("done"),
   error: value => console.log(value)
