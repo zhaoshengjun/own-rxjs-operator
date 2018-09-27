f75c07538e56388ca898bf721ea87e368a5e20a4
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-27 22:00:08 +1000

    map subscriber

--- src/map.js
+++ src/map.js
@@ -1,6 +1,14 @@
+import { Subscriber } from "rxjs";
+
+class MapSubscriber extends Subscriber {
+  _next(value) {
+    this.destination.next(value * 2);
+  }
+}
+
 export const map = fn => source =>
   source.lift({
     call(sub, source) {
-      source.subscribe(sub);
+      source.subscribe(new MapSubscriber(sub));
     }
   });
