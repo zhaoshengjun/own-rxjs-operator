ee934b8e86840893cc2cb311d67b0b3ed2b0a61b
Sam Zhao <zhao.shengjun@gmail.com>
2018-09-27 22:11:03 +1000

    use delay

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { fromEvent, of, pipe } from "rxjs";
-import { scan } from "rxjs/operators";
+import { scan, delay } from "rxjs/operators";
 
 const observable$ = fromEvent(document, "click").pipe(scan(i => i + 1, 0));
 
 const subscriber = {
   next: value => {
     console.log(value);
+    of(value)
+      .pipe(delay(500))
+      .subscribe({
+        next: value => console.log(`inner ${value}`)
+      });
   },
   complete: () => console.log("done"),
   error: value => console.log(value)
